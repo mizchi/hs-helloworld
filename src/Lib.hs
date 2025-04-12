a8f535c7e14213b3eda9433a88a237729dfca682
mizchi <miz404@gmail.com>
2025-04-12 20:31:57 +0900

    add quicksort

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,11 @@
-module Lib where
+module Lib (add, quicksort) where
 add :: Int -> Int -> Int
 add x y = x + y
+
+-- クイックソート関数
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = [] -- 空リストの場合は空リストを返す
+quicksort (x:xs) = -- 先頭要素 x と残りのリスト xs に分割
+    let smallerSorted = quicksort [a | a <- xs, a <= x] -- x以下の要素を再帰的にソート
+        biggerSorted  = quicksort [a | a <- xs, a > x]  -- xより大きい要素を再帰的にソート
+    in  smallerSorted ++ [x] ++ biggerSorted -- ソート済みリストを結合
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,8 +5,28 @@ import qualified System.Exit as Exit
 test1 :: Test
 test1 = TestCase (assertEqual "should return 3" 3 (add 1 2))
 
+-- クイックソートのテストケース
+quickSortTestEmpty :: Test
+quickSortTestEmpty = TestCase (assertEqual "for empty list" ([] :: [Int]) (quicksort []))
+
+quickSortTestSorted :: Test
+quickSortTestSorted = TestCase (assertEqual "for sorted list" ([1, 2, 3] :: [Int]) (quicksort ([1, 2, 3] :: [Int])))
+
+quickSortTestReverse :: Test
+quickSortTestReverse = TestCase (assertEqual "for reverse sorted list" ([1, 2, 3] :: [Int]) (quicksort ([3, 2, 1] :: [Int])))
+
+quickSortTestRandom :: Test
+quickSortTestRandom = TestCase (assertEqual "for random list" ([1, 1, 2, 3, 4, 5, 6, 9] :: [Int]) (quicksort ([3, 1, 4, 1, 5, 9, 2, 6] :: [Int])))
+
+-- 全テストをまとめる
 tests :: Test
-tests = TestList [TestLabel "test1" test1]
+tests = TestList [
+    TestLabel "add test" test1,
+    TestLabel "quicksort empty" quickSortTestEmpty,
+    TestLabel "quicksort sorted" quickSortTestSorted,
+    TestLabel "quicksort reverse" quickSortTestReverse,
+    TestLabel "quicksort random" quickSortTestRandom
+    ]
 
 main :: IO ()
 main = do
